05f33942cf680329b5fcad0c7d9448d4cdac2988
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-04-16 19:30:51 -0700

    commit all

--- stringExercise/Program.cs
+++ stringExercise/Program.cs
@@ -10,6 +10,29 @@ namespace stringExercise
     {
         static void Main(string[] args)
         {
+            //string stringOne = "Three Hundred";
+            //string stringTwo = " And Seventy-six";
+
+            //Console.WriteLine(stringOne + stringTwo);
+            //Console.ReadLine();
+
+            //string randomCase = "thEReS A bEAr outSiDE.";
+
+            //string upper = randomCase.ToUpper();
+            //Console.WriteLine(upper);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("\t There's a bear outside.");
+            sb.Append("\n Here come 375 more!");
+            sb.Append("\n Now there are three hundred and seventy-six.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
         }
     }
 }
